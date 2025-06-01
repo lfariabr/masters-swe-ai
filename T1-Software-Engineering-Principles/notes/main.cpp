51e02d3e3cf30ddcf78216bebd21527882120f84
Luis Faria <lfariabr@gmail.com>
2025-06-02 08:41:16 +1000

    sep_studying - first c++ code

---
+++ T1-Software-Engineering-Principles/notes/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+// Compile with: g++ -o hello main.cpp
+// Run with: ./hello
+
+// C++ is a compiled language, so it needs to be compiled before it can be run.
+// The g++ compiler is used to compile C++ code.
+// For bigger projects, we can use make or CMake to manage the build process.
