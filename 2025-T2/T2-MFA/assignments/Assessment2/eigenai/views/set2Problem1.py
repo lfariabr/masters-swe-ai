e62d09240a3ab10f0e7ac33a95c852c9c0306965
Luis Faria <lfariabr@gmail.com>
2025-10-26 06:49:49 -0300

    sync up

--- 2025-T2/T2-MFA/assignments/Assessment2/eigenai/views/set2Problem1.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def display_s2p1():
-    st.title("📘 Integration Concepts")
-    st.caption("Set 2 – Problem 1")
-    
-    st.info("This page will explore integration fundamentals in MFA501.")
